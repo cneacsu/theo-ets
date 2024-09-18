d4ef4fd169e38c396b7a9c9baa2e5008bd5f7dd5
Catalin Neacsu <cneacsu@hotmail.com>
2024-09-17 22:24:37 -0400

    2nd commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "orders.h"
+
+int main(int argc, char *argv[])
+{
+  double order_subtotal = 50.00;
+
+  double grand_total = calculate_order_total(order_subtotal);
+
+  printf("Grand total: %lf", grand_total);
+}
